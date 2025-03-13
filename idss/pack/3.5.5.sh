ff729b76747b395c7cd101f169f44bdf714511f9
moesn <475484467@qq.com>
2025-03-13 11:10:24 +0800

    no message

--- idss/pack/3.5.5.sh
+++ idss/pack/3.5.5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 配置信息
-PACKAGES=("dsmcp-fe@3.5.5")
+PACKAGES=("dist@3.5.0")
 #PACKAGES=("dist@3.5.0" "dsmcp-fe@3.5.5" "dsmc-fe@3.5.5" "idm-fe@3.5.5" "sdd-fe@3.5.5")
 REMOTE_HOSTS=("192.168.30.81")
 #REMOTE_HOSTS=("192.168.30.41" "192.168.30.51" "192.168.30.62" "192.168.30.81")
